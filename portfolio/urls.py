21be2a1805209a13d3792afed2949e1a64f42e59
avinashz14 <prajapati143a@gmail.com>
2021-02-17 15:33:09 +0530

    make it better

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from display import views
+from blog import blogviews
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home , name='home'),
+    path('', blogviews.home , name='home'),
     path('blog/', include('blog.urls')),
     path('projects/', include('projects.urls')),
 
