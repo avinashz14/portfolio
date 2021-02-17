3a4d73ab9e72f67e293cac877185773b14a6b025
avinashz14 <prajapati143a@gmail.com>
2021-02-17 14:44:09 +0530

    abc

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'display.apps.DisplayConfig',
-    'blog.apps.BlogConfig',
+    'display',
+    'blog',
     'projects'
 ]
 
