44448f7c04858ce1509e318ea13511d6c6a925e6
avinashz14 <prajapati143a@gmail.com>
2021-02-17 16:48:46 +0530

    make it better

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '0apwu^*axv7o&vft%tx7g&bs(dxfxa5+eyc02aw8p!&908t!4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','avinash-portfolio.herokuapp.com', 'portfolio-avinash.herokuapp.com']
 
