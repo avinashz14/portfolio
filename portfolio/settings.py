018ba371829a0e05657932134db02135e63de5fc
avinashz14 <prajapati143a@gmail.com>
2021-02-17 14:53:01 +0530

    make it better

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', '..//templates'],
+        'DIRS': ['templates', './/display/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
