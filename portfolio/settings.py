63e91564fdd66126f1c2f89490a692fde88f5ee3
avinashz14 <prajapati143a@gmail.com>
2021-03-23 01:25:24 +0530

    config improvement

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -160,15 +160,14 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 '''
-SENDGRID_API_KEY= config('SENDGRID_API_KEY')
+SENDGRID_API_KEY = config("SENDGRID_API_KEY")
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_HOST_USER = 'your sendgrid user name'
+EMAIL_HOST_PASSWORD = 'your sendgrid password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'your sendgrid email'
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-SENDGRID_SANDBOX_MODE_IN_DEBUG = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 #media files 
