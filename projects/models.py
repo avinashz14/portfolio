c5ddd2783280bff55e01bf98d2bf061ef4e945f7
avinashz14 <prajapati143a@gmail.com>
2021-05-02 00:36:28 +0530

    project details added

--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.ImageField(upload_to='pro_img/')
+    url = models.URLField(max_length=200, default='#')
     
     def __str__(self):
         return self.title
